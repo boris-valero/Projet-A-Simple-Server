02b298bf4ee2fc716e97aacdc4384146470980f6
Ordinateur <val_b@orange.fr>
2024-06-14 12:24:55 +0200

    fifth commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/", (req, res, next)=>{
-//    res.send("This is a simple response");
-res.render("index");
+const axios = require("axios");
+router.get("/", (req, res, next) => {
+  //    res.send("This is a simple response");
+  axios
+    .get("https://www.affirmations.dev/")
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.render("index");
 });
 
 module.exports = router;
-
